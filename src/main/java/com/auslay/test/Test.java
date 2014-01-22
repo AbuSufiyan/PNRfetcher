5016bb5680f4d1bb7a9002818598de025c1d79e6
abu <abu@qburst.com>
2014-01-22 09:30:18 +0530

    modified url requests

--- src/main/java/com/auslay/test/Test.java
+++ src/main/java/com/auslay/test/Test.java
@@ -3,21 +3,16 @@ package com.auslay.test;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.zip.GZIPInputStream;
 
-import org.apache.commons.io.IOUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -34,20 +29,18 @@ public class Test {
 
 			args = checkIfFileSuppliedForPNR(args);
 			for (int count = 0; count < args.length; count++) {
-				
+
 				pnr = args[count];
 
 				body = FetchCurrentStatus(pnr);
-				passengerDetails.append(FetchDetails(body, pnr,count==0));
+				passengerDetails.append(FetchDetails(body, pnr, count == 0));
 
 			}
 			// System.out.println(body);
 
 		} else {
-			passengerDetails
-					.append("<h2>PNR is empty.</h2></br> sample: java -jar Test 4810991753");
-			System.out
-					.println("PNR missing. sample: java -jar Test 4810991753.");
+			passengerDetails.append("<h2>PNR is empty.</h2></br> sample: java -jar Test 4810991753");
+			System.out.println("PNR missing. sample: java -jar Test 4810991753.");
 		}
 
 		File new03 = new File("Status.html");
@@ -56,26 +49,38 @@ public class Test {
 		fileWriter.flush();
 		fileWriter.close();
 
-		ProcessBuilder pBuilder = new ProcessBuilder("cmd.exe", "/c", "start "
-				+ "Status.html");
-		pBuilder.redirectErrorStream(true);
-		Process startDirectory = pBuilder.start();
+		if ("Linux".equalsIgnoreCase(System.getProperty("os.name"))) {
+			// ProcessBuilder pBuilder = new ProcessBuilder("cmd.exe", "/c", "start " + "Status.html");
+			// pBuilder.redirectErrorStream(true);
+			// Process startDirectory = pBuilder.start();
+
+			Runtime runtime = Runtime.getRuntime();
+			try {
+				runtime.exec("xdg-open " + "Status.html");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} else {
+			ProcessBuilder pBuilder = new ProcessBuilder("cmd.exe", "/c", "start " + "Status.html");
+			pBuilder.redirectErrorStream(true);
+			Process startDirectory = pBuilder.start();
+		}
 
 	}
 
-	private static String[] checkIfFileSuppliedForPNR(String[] args)
-			throws IOException {
+	private static String[] checkIfFileSuppliedForPNR(String[] args) throws IOException {
 		if (args.length == 1) {
-			if (args[0].contains("txt")) {
+			if (args[0].contains("txt"))
 				return parseTextForPnr(args[0]);
-			}
 		}
 		return args;
 
 	}
 
 	private static String[] parseTextForPnr(String fileName) throws IOException {
-		ArrayList pnrList = new ArrayList();
+		ArrayList<String> pnrList = new ArrayList<String>();
 		FileReader pnrReader = new FileReader(fileName);
 		BufferedReader reader = new BufferedReader(pnrReader);
 		String pnr = reader.readLine();
@@ -85,101 +90,132 @@ public class Test {
 			pnr = reader.readLine();
 		}
 		reader.close();
-		//String[] args = new String[pnrList.size()];
+		// String[] args = new String[pnrList.size()];
 		return (String[]) pnrList.toArray(new String[pnrList.size()]);
 
 	}
 
-	private static StringBuilder FetchDetails(String body, String PNR, boolean firstEntry)
-			throws Exception {
-		
+	private static StringBuilder FetchDetails(String body, String PNR, boolean firstEntry) throws Exception {
+
 		StringBuilder passengerDetails = new StringBuilder();
 		// Empty response
 		if (body == null) {
 			passengerDetails
-					.append("<h2>Could not receive response.Check INternet Connection.</h2></br> sample: java -jar Test 4810991753");
+			    .append("<h2>Could not receive response.Check INternet Connection.</h2></br> sample: java -jar Test 4810991753");
 			return passengerDetails;
 
 		}// PNR was invalid
 		else if (body.contains("Following ERROR was encountered")) {
-			passengerDetails
-					.append("</br></br><b style=\"color:red\">Incorrect PNR : "
-							+ PNR + "</b>");
+			passengerDetails.append("</br></br><b style=\"color:red\">Incorrect PNR : " + PNR + "</b>");
 			System.out.println("Incorrect PNR");
 			return passengerDetails;
 		}// Valid Data Found
 		else {
 			Document doc = Jsoup.parse(body);
-			//Parse passenger details
+			// Parse passenger details
 			Element element = doc.getElementById("center_table");
-			//Parse train details
+			// Parse train details
 			Elements trainElements = doc.getElementsByClass("table_border_both");
-//			doc.get
+			// doc.get
 			String trainDetails = getTrainDetails(trainElements);
-						
+
 			String data = element.text();
-			return DisplayDetails(data, PNR, firstEntry,trainDetails);
+			return DisplayDetails(data, PNR, firstEntry, trainDetails);
 
 		}
 
 	}
 
-	private static String getTrainDetails(Elements trainElements)
-	{
+	private static String getTrainDetails(Elements trainElements) {
 		StringBuilder trainDetails = new StringBuilder();
-		int i=0;
+		int i = 0;
 		for (Element element : trainElements) {
-			
+
 			switch (i) {
-			case 0:trainDetails.append("" +element.text());i++;break;
-			case 1:trainDetails.append(" - "+element.text());i++;break;
-			case 2:trainDetails.append(" \nJourney : <b>"+element.text()+"</b>");i++;break;
-			case 3:trainDetails.append(" , "+element.text());i++;break;
-			case 4:trainDetails.append(" ->  "+element.text());i++;break;
-			case 7:trainDetails.append("\nClass \t\t: "+element.text()+"\n\n");i++;break;
-			default:
-				i++;break;
+				case 0:
+					trainDetails.append("" + element.text());
+					i++;
+					break;
+				case 1:
+					trainDetails.append(" - " + element.text());
+					i++;
+					break;
+				case 2:
+					trainDetails.append(" \nJourney : <b>" + element.text() + "</b>");
+					i++;
+					break;
+				case 3:
+					trainDetails.append(" , " + element.text());
+					i++;
+					break;
+				case 4:
+					trainDetails.append(" ->  " + element.text());
+					i++;
+					break;
+				case 7:
+					trainDetails.append("\nClass \t\t: " + element.text() + "\n\n");
+					i++;
+					break;
+				default:
+					i++;
+					break;
 			}
 		}
 		return trainDetails.toString();
 	}
-	
-	
+
 	private static String FetchCurrentStatus(String pnr) throws IOException {
-		URL url = new URL(
-				"http://www.indianrail.gov.in/cgi_bin/inet_pnrstat_cgi.cgi");
+		URL url = new URL("http://www.indianrail.gov.in/cgi_bin/inet_pnstat_cgi_28688.cgi");
+		url = new URL("http://www.indianrail.gov.in/cgi_bin/inet_pnstat_cgi_28688.cgi");
 		HttpURLConnection con = (HttpURLConnection) url.openConnection();
 		con.setDoOutput(true);
+		// con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+		con.setRequestProperty("Accept-Encoding", "gzip, deflate");
+		con.setRequestProperty("Referer", "http://www.indianrail.gov.in/pnr_Enq.html");
 
-		String urlParams = "lccp_cap_val=123&lccp_capinp_val=123&lccp_pnrno1=" + pnr + "&submit=Get+Status";
+		String urlParams = "lccp_cap_val=1234&lccp_capinp_val=1234&lccp_pnrno1=" + pnr + "&submit=Get+Status";
 		// 4810991753
-		DataOutputStream dOutStream = new DataOutputStream(
-				con.getOutputStream());
+		DataOutputStream dOutStream = new DataOutputStream(con.getOutputStream());
 		dOutStream.writeBytes(urlParams);
 		dOutStream.flush();
 		dOutStream.close();
 
-		InputStream in = con.getInputStream();
+		InputStream in = new GZIPInputStream(con.getInputStream());
 		String encoding = con.getContentEncoding();
 		encoding = encoding == null ? "UTF-8" : encoding;
-		return IOUtils.toString(in, encoding);
+
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			byte[] buffer = new byte[6553];
+			int noRead;
+
+			while ((noRead = in.read(buffer)) > 0) {
+				// System.out.write(buffer, 0, noRead);
+				sb.append(new String(buffer));
+			}
+		} finally {
+			in.close();
+		}
+
+		// String gh = IOUtils.toString(in, encoding);
+		// in.close();
+		return sb.toString();
 	}
 
-	private static StringBuilder DisplayDetails(String data, String PNR,
-			boolean firstEntry, String trainDetails) throws Exception {
+	private static StringBuilder DisplayDetails(String data, String PNR, boolean firstEntry, String trainDetails)
+	    throws Exception {
 		String result = data.substring(data.indexOf("Passenger"));
-		String passenegerStatusString = result.substring(0,
-				result.indexOf("Charting Status"));
+		String passenegerStatusString = result.substring(0, result.indexOf("Charting Status"));
 		StringBuilder sb = new StringBuilder();
 
 		Pattern p = Pattern.compile("Passenger");
-		Matcher m = p.matcher(result);
 		int index = passenegerStatusString.indexOf("Passenger");
 		int startindex = 0;
 		if (!firstEntry) {
 			sb.append("\n\n----------------------------------\n\n");
 		}
-		
+
 		sb.append(trainDetails);
 		sb.append("PNR: " + PNR);
 		while (index >= 0) {
